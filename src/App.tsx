5bc1db08d0688efd2ae9437088660e1f1a9ea710
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-07-27 00:19:26 -0300

    feat: router and pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,19 @@
+import { BrowserRouter } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
+
+import { defaultTheme } from "./styles/themes/default";
+import { GlobalStyles } from "./styles/global";
+
+import { Router } from "./Router";
+
 export function App() {
   return (
-    <h1>Coffee</h1>
+    <ThemeProvider theme={defaultTheme}>
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
+
+      <GlobalStyles />
+    </ThemeProvider>
   )
 }
