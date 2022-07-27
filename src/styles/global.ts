2cd7e5de6a8416ef74a33b3675bfb04d94772562
Pedro Lopes <pedrolopesgrc@gmail.com>
2022-07-27 00:10:49 -0300

    init: first commit

---
+++ src/styles/global.ts
@@ -0,0 +1,26 @@
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyles = createGlobalStyle`
+  * {
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+  }
+
+  :focus {
+    outline: 0;
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['yellow-700']};
+  }
+
+  body {
+    background: ${({ theme }) => theme['gray-100']};
+    color: ${({ theme }) => theme['gray-700']};
+    -webkit-font-smoothing: antialiased;
+  }
+
+  body, input, textarea, button {
+    font-family: "Roboto", sans-serif;
+    font-weight: 400;
+    font-size: 1rem;
+  }
+`
